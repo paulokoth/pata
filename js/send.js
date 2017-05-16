016c55a9cf765301ac62f13b50a79075d7d7ffe0
collo <qollomcbizzy64@gmail.com>
2017-05-16 19:13:02 +0300

    added the sign up method

--- js/send.js
+++ js/send.js
@@ -29,4 +29,28 @@ methods: {
       person.auth.hasErrors = true;
     })
   }
+  //sign up for user
+  signup:function(event){
+    var person =this;
+    person.auth.message= '';
+    person.auth.hasErrors = false;
+    if(person.auth.useremail === '')||person.auth.pass === ''){
+      alert('Please provide the email and the password');
+      return;
+    }
+    //create new user
+    firebase.auth().signInWithEmailAndPassword(person.auth.useremail, person.auth.pass)
+    .then(function(data){
+      person.auth.user = firebase.auth().currentUser;
+    }).catch(function(error){
+      person.auth.message ='successfully created new user';
+      person.auth.user= firebase.auth().currentUser;
+      person.auth.useremail='';
+      person.auth.pass='';
+      person.auth.username='';
+    }).catch(function(error){
+      person.auth.message =error.message;
+      person.auth.hasErrors = true;
+    });
+  }
 }
