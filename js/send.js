c339f38fe5c42df42ab40c8041cc410ebba86363
collo <qollomcbizzy64@gmail.com>
2017-05-16 19:04:40 +0300

    added the log in method

--- js/send.js
+++ js/send.js
@@ -11,3 +11,22 @@ var app=new Vue({
     }
   },
 })
+methods: {
+  //autheticate the user
+  login: function(event){
+    var person = this;
+    person.auth.message= '';
+    person.auth.hasErrors = false;
+    if(person.auth.useremail === '')||person.auth.pass === ''){
+      alert('Please provide the email and the password');
+      return;
+    }
+    firebase.auth().signInWithEmailAndPassword(person.auth.useremail, person.auth.pass)
+    .then(function(data){
+      person.auth.user = firebase.auth().currentUser;
+    }).catch(function(error){
+      person.auth.message =error.message;
+      person.auth.hasErrors = true;
+    })
+  }
+}
