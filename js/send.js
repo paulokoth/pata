d8fbbe5898fd7961373b8974e61c38707c30de45
collo <qollomcbizzy64@gmail.com>
2017-05-16 18:51:05 +0300

    added the properties for the vue

---
+++ js/send.js
@@ -0,0 +1,13 @@
+var app=new Vue({
+  el : '#app',
+  data : {
+    auth: {
+      user: null,
+      useremail: '',
+      pass: '';
+      message: '';
+      username:'';
+      hasErrors:false
+    }
+  },
+})
