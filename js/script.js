6c8d87396c1db1370fb97b833fed22168e7aa314
collo <qollomcbizzy64@gmail.com>
2017-05-16 10:31:05 +0300

    added the user interface for the password and confirmation for the password

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ $(document).ready(function(){
   });
   $("button#signup").click(function(){
   $(".modal2").slideDown();
+  $("form#signup").submit(function(event){
+  var inputtedPassword=String(parseInt($("input#password")).val());
+  var inputtedConfirmation=String(parseInt($("input#confirm")).val());
+   });
   $("#closeit").click(function(){
     $(".modal2").hide();
   });
