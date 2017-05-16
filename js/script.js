e193628441328016c5dabc2a5b8ff9d739f4f194
collo <qollomcbizzy64@gmail.com>
2017-05-16 10:16:47 +0300

    added the initial commit with the modal

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+  $("button#logintop").click(function(){
+  $(".modal").slideDown();
+  $("#close").click(function(){
+    $(".modal").hide();
+  });
+  });
+  $("button#signup").click(function(){
+  $(".modal2").slideDown();
+  $("#closeit").click(function(){
+    $(".modal2").hide();
+  });
+  });
+  });
