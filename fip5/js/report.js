7f9a1ac35816d7162c43f1cb9b4cca7d0f94b62d
victor tarus <victorkibet25@gmail.com>
2017-05-18 16:33:07 +0300

    added script cdn

--- fip5/js/report.js
+++ fip5/js/report.js
@@ -1,10 +1,8 @@
-<script>
-    function received() {
-  alert("you report has been received");
-     var modal=document.getElementById('mymodal');
-     modal.style.display="none";
-     var theMessage=document.getElementById('form#messaging');
-     theMessage.reset();
-  return false;
-  }
-    </script>
+$(document).ready(function(){
+ $("button#message").click(function(){
+   $(".messagemodal").show();
+   $("form#messaging").submit(function(){
+       $(".messagemodal").hide();
+   });
+ });
+ });
