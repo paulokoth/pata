25ee5bfdee99135e8e5e8dd9358c2a1768207692
victor tarus <victorkibet25@gmail.com>
2017-05-18 16:13:08 +0300

    added report js

---
+++ fip5/js/report.js
@@ -0,0 +1,10 @@
+<script>
+    function received() {
+  alert("you report has been received");
+     var modal=document.getElementById('mymodal');
+     modal.style.display="none";
+     var theMessage=document.getElementById('form#messaging');
+     theMessage.reset();
+  return false;
+  }
+    </script>
